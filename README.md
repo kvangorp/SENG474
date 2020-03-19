9e52c6ac34a47f3e41c6f23ce7b9f3ce82b8c7c7
kvangorp <katelyn.vangorp@gmail.com>
2020-03-18 17:44:00 -0700

    Remove stop words and use english dictionary

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# SENG474
+# SENG474 Project
+
+## Data Processing
+
+- To run the data processing script, you need to have newspaper3k and nltk installed. It takes about an hour to run but it outputs a csv file with the processed data, so we won't need to run it each time we train our models.
+- The cleaned and processed data can be found in `data/processed-dataset.csv`
+- Each row of the csv file contains the number of occurrences of each english word in an article, excluding stop words
+- The last column is the label and is 1 is the article is real, 0 if the article is fake.
+- I was having problems with stemming the words, but we can take another crack at this if we need to reduce our number of features
+- We could also try using sklearn's `TfidfVectorizer` instead `CountVectorizer` later to see if that improves our accuracy
+
+## Attribution
+
+- Article text extraction: [Newspaper3k](https://newspaper.readthedocs.io/en/latest/)
+- List of english words and stopwords from the [NLTK Corpus](http://www.nltk.org/data.html)
+- Tokenizer and stemming function inspired by [Jonathan Soma's blog](http://jonathansoma.com/lede/algorithms-2017/classes/more-text-analysis/counting-and-stemming/)
---
+++ code/process_data.py
@@ -0,0 +1,139 @@
+import pandas as pd
+import numpy as np
+from newspaper import Article
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import words, stopwords
+import re
+
+
+def main():
+
+    # Process and combine datasets
+    combined_dataset = retrieve_data()
+
+    # Extract article text from URLs
+    extracted_text_dataset = extract_text(combined_dataset)
+
+    # Extract vocabulary and word counts from article text
+    processed_text_dataset = process_text(extracted_text_dataset)
+
+    # Store processed dataset in a CSV
+    processed_text_dataset.to_csv('../data/processed-dataset.csv', index=False, header=True)
+
+    print(processed_text_dataset)
+
+
+
+def retrieve_data():
+
+    pd.options.mode.chained_assignment = None # disable warning
+
+    print('PROCESSING AD-FONTS-MEDIA DATA...')
+
+    # Read in ad-fonts dataset
+    path = '../data/ad-fontes-media.csv'
+    df = pd.read_csv(path, delimiter = ',', header=0)
+    df = df.rename(columns={'Quality': 'Truth'})
+
+    # Extract relevant columns
+    ad_fontes_data = df[['Url', 'Truth']]
+
+    # Turn truth value into a binary class
+    ad_fontes_data['Truth'] = np.where(ad_fontes_data['Truth'] > 24, 1, 0)
+
+
+
+    print('PROCESSING POLITIFACT DATA...')
+
+    # Read in politifact datasets
+    fake_data = pd.read_csv('../data/politifact_fake.csv', delimiter = ',', header=0)
+    real_data = pd.read_csv('../data/politifact_real.csv', delimiter = ',', header=0)
+
+    # Add truth column
+    fake_data['Truth'] = 0
+    real_data['Truth'] = 1
+
+    # Combine politifact datasets
+    politifact_data = pd.concat([fake_data, real_data]).reset_index(drop=True)
+
+    # Extract relevent columns
+    politifact_data = politifact_data.rename(columns={'news_url':'Url'})
+    politifact_data = politifact_data[['Url', 'Truth']]
+
+
+    # Combine datasets
+    all_data = pd.concat([ad_fontes_data, politifact_data]).reset_index(drop=True)
+
+    return all_data
+
+
+
+def extract_text(original_dataset):
+
+    print('EXTRACTING ARTICLE TEXT FROM URLS...')
+
+    new_dataset = pd.DataFrame(columns=['Url', 'ArticleText', 'Truth'])
+
+    for row in range(len(original_dataset.index)):
+
+        example = {}
+
+        try:
+            # Extract article text from url
+            url = original_dataset.at[row, 'Url']
+            article = Article(url=url, MIN_WORD_COUNT=300, fetch_images=False, http_success_only=True, language='en')
+            article.download()
+            article.parse()
+            print('Retrieved:', article.title)
+
+            # Add new row to dataset
+            example['Url'] = url
+            example['ArticleText'] = article.text
+            example['Truth'] =  original_dataset.at[row, 'Truth']
+            new_dataset = new_dataset.append(example, ignore_index=True)
+
+        except Exception as e:
+            # Skip to next row if the article was not able to be retrieved
+            print('Exception at row ', row, ': ', e)
+            continue
+
+    return new_dataset
+
+
+def process_text (dataset):
+
+    print("PROCESSING ARTICLE TEXT...")
+
+    # Get list of stopwords
+    nltk.download('stopwords')
+    stops = set(stopwords.words('english'))
+
+    # Get list of english words
+    nltk.download('words')
+    english_words = np.unique(words.words())
+
+    # Extract vocabulary from article text
+    vectorizer = CountVectorizer(stop_words=stops, tokenizer=tokenizer, vocabulary=english_words)
+    X = vectorizer.fit_transform(dataset['ArticleText'])
+    
+    # Create new dataframe with vocabulary and labels
+    vect_df = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names())
+    new_dataset = pd.concat([vect_df, dataset['Truth']], axis=1)
+
+    # Remove empty columns
+    new_dataset = new_dataset.loc[:, (new_dataset != 0).any(axis=0)]
+
+    return new_dataset
+
+
+def tokenizer (text):
+    porter_stemmer = PorterStemmer()
+    tokens = re.sub(r"[^A-Za-z0-9\-]", " ", text).lower().split()
+    # stemmed_words = [porter_stemmer.stem(word) for word in tokens]
+    return tokens
+
+
+if __name__ == '__main__':
+    main()
