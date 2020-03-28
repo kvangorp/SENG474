b434322c45200e10775979faf1256fd4667030e7
kvangorp <katelyn.vangorp@gmail.com>
2020-03-28 14:50:01 -0700

    Add more fake news data

--- code/analyze_data.py
+++ code/analyze_data.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 
+
 def main():
 
-    dataset = pd.read_csv('../data/processed-dataset.csv', delimiter = ',', header=0)
+    dataset = pd.read_csv('../data/processed-dataset.csv',
+                          delimiter=',', header=0)
 
     print(dataset.loc[dataset['Truth'] == 0])
-
-
+    print(dataset.loc[dataset['Truth'] == 1])
 
 
 if __name__ == '__main__':
--- code/process_data.py
+++ code/process_data.py
@@ -20,21 +20,21 @@ def main():
     processed_text_dataset = process_text(extracted_text_dataset)
 
     # Store processed dataset in a CSV
-    processed_text_dataset.to_csv('../data/processed-dataset.csv', index=False, header=True)
+    processed_text_dataset.to_csv(
+        '../data/processed-dataset.csv', index=False, header=True)
 
     print(processed_text_dataset)
 
 
-
 def retrieve_data():
 
-    pd.options.mode.chained_assignment = None # disable warning
+    pd.options.mode.chained_assignment = None  # disable warning
 
     print('PROCESSING AD-FONTS-MEDIA DATA...')
 
     # Read in ad-fonts dataset
     path = '../data/ad-fontes-media.csv'
-    df = pd.read_csv(path, delimiter = ',', header=0)
+    df = pd.read_csv(path, delimiter=',', header=0)
     df = df.rename(columns={'Quality': 'Truth'})
 
     # Extract relevant columns
@@ -43,13 +43,13 @@ def retrieve_data():
     # Turn truth value into a binary class
     ad_fontes_data['Truth'] = np.where(ad_fontes_data['Truth'] > 24, 1, 0)
 
-
-
     print('PROCESSING POLITIFACT DATA...')
 
     # Read in politifact datasets
-    fake_data = pd.read_csv('../data/politifact_fake.csv', delimiter = ',', header=0)
-    real_data = pd.read_csv('../data/politifact_real.csv', delimiter = ',', header=0)
+    fake_data = pd.read_csv(
+        '../data/politifact_fake.csv', delimiter=',', header=0)
+    real_data = pd.read_csv(
+        '../data/politifact_real.csv', delimiter=',', header=0)
 
     # Add truth column
     fake_data['Truth'] = 0
@@ -59,15 +59,29 @@ def retrieve_data():
     politifact_data = pd.concat([fake_data, real_data]).reset_index(drop=True)
 
     # Extract relevent columns
-    politifact_data = politifact_data.rename(columns={'news_url':'Url'})
+    politifact_data = politifact_data.rename(columns={'news_url': 'Url'})
     politifact_data = politifact_data[['Url', 'Truth']]
 
+    print('PROCESSING GOSSIPCOP DATA...')
+
+    # Read in gossipcop datasets
+    gossipcop_data = pd.read_csv(
+        '../data/gossipcop_fake.csv', delimiter=',', header=0)
+
+    # Add truth column
+    gossipcop_data['Truth'] = 0
+
+    # Extract relevent columns
+    gossipcop_data = gossipcop_data.rename(columns={'news_url': 'Url'})
+    gossipcop_data = gossipcop_data[['Url', 'Truth']]
 
     # Combine datasets
-    all_data = pd.concat([ad_fontes_data, politifact_data]).reset_index(drop=True)
+    politifact_and_gossipcop = pd.concat([politifact_data, gossipcop_data]
+                                         ).reset_index(drop=True)
+    all_data = pd.concat([ad_fontes_data, politifact_and_gossipcop]
+                         ).reset_index(drop=True)
 
     return all_data
-
 
 
 def extract_text(original_dataset):
@@ -83,7 +97,8 @@ def extract_text(original_dataset):
         try:
             # Extract article text from url
             url = original_dataset.at[row, 'Url']
-            article = Article(url=url, MIN_WORD_COUNT=300, fetch_images=False, http_success_only=True, language='en')
+            article = Article(url=url, MIN_WORD_COUNT=300,
+                              fetch_images=False, http_success_only=True, language='en')
             article.download()
             article.parse()
             print('Retrieved:', article.title)
@@ -91,7 +106,7 @@ def extract_text(original_dataset):
             # Add new row to dataset
             example['Url'] = url
             example['ArticleText'] = article.text
-            example['Truth'] =  original_dataset.at[row, 'Truth']
+            example['Truth'] = original_dataset.at[row, 'Truth']
             new_dataset = new_dataset.append(example, ignore_index=True)
 
         except Exception as e:
@@ -102,7 +117,7 @@ def extract_text(original_dataset):
     return new_dataset
 
 
-def process_text (dataset):
+def process_text(dataset):
 
     print("PROCESSING ARTICLE TEXT...")
 
@@ -115,9 +130,10 @@ def process_text (dataset):
     english_words = np.unique(words.words())
 
     # Extract vocabulary from article text
-    vectorizer = CountVectorizer(stop_words=stops, tokenizer=tokenizer, vocabulary=english_words)
+    vectorizer = CountVectorizer(
+        stop_words=stops, tokenizer=tokenizer, vocabulary=english_words)
     X = vectorizer.fit_transform(dataset['ArticleText'])
-    
+
     # Create new dataframe with vocabulary and labels
     vect_df = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names())
     new_dataset = pd.concat([vect_df, dataset['Truth']], axis=1)
@@ -128,7 +144,7 @@ def process_text (dataset):
     return new_dataset
 
 
-def tokenizer (text):
+def tokenizer(text):
     porter_stemmer = PorterStemmer()
     tokens = re.sub(r"[^A-Za-z0-9\-]", " ", text).lower().split()
     # stemmed_words = [porter_stemmer.stem(word) for word in tokens]
